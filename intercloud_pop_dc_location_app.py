f430b4b400eb4b2b70e41448fe0a871aad443b60
Reda Merzouki <reda.merzouki@yahoo.fr>
2022-05-22 10:49:18 +0200

    Fifth commit

--- intercloud_pop_dc_location_app.py
+++ intercloud_pop_dc_location_app.py
@@ -10,16 +10,17 @@ st.set_page_config(layout='wide')
 
 # Plotting Data Centers and POP Locations
 
-fig = px.scatter_geo(data,
-                    lat='latitude',
-                    lon='longitude',
-                    color = data.columns[0],
-                    size = 'number',
-                    hover_name="name",
-                    title = 'InterCloud & Partners locations <br>(Hover for locations names)',
-                    width=1000,
-                    height=600,
-                    projection="natural earth1")
-
+fig = px.scatter_mapbox(data, 
+                        lat="latitude", 
+                        lon="longitude", 
+                        hover_name="name",
+                        size = 'number', 
+                        color = data.columns[0], 
+                        zoom=3, 
+                        height=600, 
+                        width=1000
+                       )
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0},title = '<br>InterCloud & Partners locations <br>(Hover for locations names)')
 
 st.plotly_chart(fig, use_container_width=True)
